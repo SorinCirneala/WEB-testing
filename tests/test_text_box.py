a59c80afdb97b4b8fe1aca56fe2fbbcaff505b40
Sorin <sorin.cirneala@gmail.com>
2023-05-30 17:59:57 +0300

    removed unused TC, added new TC

--- tests/test_text_box.py
+++ tests/test_text_box.py
@@ -1,27 +1,21 @@
-import sys, os
-project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, project_path)
-
 import pytest
 from time import sleep
-from pages.textbox_page import TextboxPage, TB_URL
+from pages.textbox_page import TextboxPage
 
-@pytest.mark.usefixtures("driver", "login")
-@pytest.mark.smoke
 class TestTextbox:
+    @pytest.mark.usefixtures("driver")
+    @pytest.mark.smoke
     def test_sumbit_fullname(self, driver):
-        page = TextboxPage(driver)
-        page.open(TB_URL)
-        page.enter_full_name("John Connor")
-        page.enter_email
+        driver = TextboxPage(driver)
+        driver.load_page()
+        driver.enter_full_name("John Connor")
         sleep(3)
-
-
-    # def test_logout(self, driver):
-    #     # Create an instance of the DashboardPage class
-    #     text_box_page = DashboardPage(driver)
-
-    #     # Perform actions and assertions specific to the dashboard page
-    #     text_box_page.click_logout_button()
-
-    #     # Add assertions to verify the logout behavior or navigation to the login page
+    
+    
+    @pytest.mark.usefixtures("driver")
+    @pytest.mark.regression
+    def test_sumbit_email(self, driver):
+        driver = TextboxPage(driver)
+        driver.load_page()
+        driver.enter_email("sarah.connor@gmail.com")
+        sleep(3)
