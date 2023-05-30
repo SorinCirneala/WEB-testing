59f96abd281dbd2caba68f1626f712d42b7b33b3
Sorin <sorin.cirneala@gmail.com>
2023-05-30 17:59:02 +0300

    reordered locators, added load_page method

--- pages/textbox_page.py
+++ pages/textbox_page.py
@@ -1,12 +1,18 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
+
+# page URL
+TEXTBOX_PAGE_URL = "https://demoqa.com/text-box"
+
 # page locators
-TB_URL = "https://demoqa.com/text-box"
 FULLNAME_INPUT = (By.ID, "userName")
 EMAIL_INPUT = (By.ID, "userEmail")
 
 class TextboxPage(BasePage):
+    # override load_page method 
+    def load_page(self):
+        return super().load_page(TEXTBOX_PAGE_URL)
 
     # Page-specific methods
     def enter_full_name(self, fullname):
