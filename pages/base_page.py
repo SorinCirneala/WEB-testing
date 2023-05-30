303876b4256aa74b57c7d921d880b84eea30a4e2
Sorin <sorin.cirneala@gmail.com>
2023-05-30 22:01:44 +0300

    added get element text

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,17 +6,26 @@ class BasePage:
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 10)
 
+
     def load_page(self, url):
         self.driver.get(url)
 
+
     def find_element(self, locator):
         return self.wait.until(EC.presence_of_element_located(locator))
+
 
     def click_element(self, locator):
         element = self.find_element(locator)
         element.click()
 
+
     def enter_text(self, locator, text):
         element = self.find_element(locator)
         element.clear()
         element.send_keys(text)
+
+
+    def get_element_text(self, locator):
+        el = self.find_element(locator)
+        return el.text
