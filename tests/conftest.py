461db10f89b9274448a6ca7e571438094b17a3d5
Sorin <sorin.cirneala@telenav.com>
2023-06-09 08:51:48 +0300

    deleted extra lines

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,12 +21,6 @@ def driver():
 
 @pytest.fixture(scope="function")
 def login(driver):
-    # Perform login actions, such as navigating to the login page,
-    # entering credentials, and submitting the form.
-    
-    # Optionally, you can return any data or objects needed for the tests
-    
+    # setup code
     yield
-    
-    # Teardown - perform any necessary cleanup after the test
-    # For example, log out or navigate to a different page.
+    # teardown code
